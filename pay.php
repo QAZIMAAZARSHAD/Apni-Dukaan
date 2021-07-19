b9b470620a1ad01fe4a9e2239b0ef8cb104d369e
Rishita Srivastava <srivastavarishita3@gmail.com>
2021-07-19 23:53:21 +0530

    Payment Gateway Integration #25

---
+++ pay.php
@@ -0,0 +1,23 @@
+<button id="rzp-button1">Pay</button>
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<script>
+var options = {
+    "key": "rzp_test_sQ7stnW0jcu6ab", // Enter the Key ID generated from the Dashboard
+    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Apni-Dukaan",
+    "description": "Test Transaction",
+    "image": "https://example.com/your_logo",
+    
+    "handler": function (response){
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature)
+    }
+};
+var rzp1 = new Razorpay(options);
+document.getElementById('rzp-button1').onclick = function(e){
+    rzp1.open();
+    e.preventDefault();
+}
+</script>
