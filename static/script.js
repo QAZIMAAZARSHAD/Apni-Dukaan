a9d44f06ab8a15e1c082518cb6a9dea2061ea426
Shreyansh Shrey <shreyansh.shrey1999@gmail.com>
2021-08-01 10:39:46 +0530

    fix: signin

--- static/script.js
+++ static/script.js
@@ -1,27 +1,26 @@
 ﻿var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0]; 
-btn.onclick = function() {
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
   modal.style.display = "block";
-}
-span.onclick = function() {
+};
+span.onclick = function () {
   modal.style.display = "none";
-}
-window.onclick = function(event) {
+};
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
 $(document).ready(() => {
-
   // Banner Owl Carousel
-  $('#owl_carousel .owl-carousel').owlCarousel({
-      items: 1,
-      dots:true,
-      loop: true,
-      autoplay:true,
-      autoplayTimeout:8000,
-      stagePadding: 0
+  $("#owl_carousel .owl-carousel").owlCarousel({
+    items: 1,
+    dots: true,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 8000,
+    stagePadding: 0,
   });
 });
